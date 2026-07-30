327989f3864186bf024d91e6c5b859954e2cc752
Junior <j.rodrigues.junior@accenture.com>
2026-07-30 16:42:50 -0300

    feat: cria infraestrutura do módulo financial dashboard
    - endpoint GET /finance/dashboard com documentação Swagger
    - FinancialDashboardService como stub
    - FinancialDashboardResponse como DTO principal
    - segurança com @RequireRole(OWNER, ADMIN, FINANCIAL)
    
    PR #1

---
+++ src/main/java/br/com/corely/financialdashboard/dto/FinancialDashboardResponse.java
@@ -0,0 +1,16 @@
+package br.com.corely.financialdashboard.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Dashboard financeiro completo com visão consolidada do estúdio")
+public class FinancialDashboardResponse {
+
+}
